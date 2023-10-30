dcdb96659bddf1bf95ceb494f356bba9aedb8e62
MaxiJorge <144859333+MaxiJorge@users.noreply.github.com>
2023-10-30 22:30:40 +0100

    ACABADO

--- Common/3.StacksAndQueues/GenericQueue.cs
+++ Common/3.StacksAndQueues/GenericQueue.cs
@@ -1,37 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Common
 {
 
     public class GenericQueue<T> : IPushPop<T>
     {
         //TODO #1: Declare a List inside this object class to store the objects. Choose the most appropriate object class
+        private GenericList<T> queue = new GenericList<T>();
 
         public string AsString()
         {
             //TODO #2: Return the list as a string. Use the method already implemented in your list
-            return null;
+            return queue.ToString();
         }
 
         public int Count()
         {
             //TODO #3: Return the number of objects
-            return 0;
+            return queue.Count();
         }
 
         public void Clear()
         {
             //TODO #4: Remove all objects stored
+            queue.Clear();
         }
 
         public void Push(T value)
         {
             //TODO #5: Add a new object to the list (at the end of it)
+            queue.Add(value);
         }
 
         public T Pop()
         {
             //TODO #6: Remove the first object of the list and return it
-
-            return default(T);
+            
+         T pop = queue.Get(0);
+         queue.Remove(0);
+         return pop;
         }
     }
 }
